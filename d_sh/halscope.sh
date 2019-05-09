da3ea7b1346efd224eb9415617b9d43d18904a8e
CoolCNC <service@thecooltool.com>
2019-05-09 15:39:11 +0200

    Halmeter u. Halscope
    start scripts

---
+++ d_sh/halscope.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [ ! "$BASH_VERSION" ] ; then
+    echo "Warning: this script should be executed with bash"
+    exec /bin/bash "$0"
+fi
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+source util/init.sh
+ssh -i $KEYFILE -Y -oBatchMode=yes -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null machinekit@192.168.7.2 "source /etc/profile; halscope"
